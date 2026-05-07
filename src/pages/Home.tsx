fda072808bb34e0372b834ac7d2ac06ee1c3a291
Muhammad Luthfi Khusyasy <44360105+Khusyasy@users.noreply.github.com>
2026-05-07 23:02:51 +0700

    chore: setup zustand

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,36 @@
-import { useState } from 'react'
+import { useCountStore } from '../stores/count'
 
 function Home() {
-  const [count, setCount] = useState(0)
+  const count = useCountStore((state) => state.count)
+  const increment = useCountStore((state) => state.increment)
+  const decrement = useCountStore((state) => state.decrement)
+  const reset = useCountStore((state) => state.reset)
+
 
   return (
     <>
       <p className="text-3xl">Test</p>
+      <p>Count is: {count}</p>
+      <button
+        type="button"
+        className="bg-red-300 rounded px-2 py-1 cursor-pointer"
+        onClick={decrement}
+      >
+        -
+      </button>
       <button
         type="button"
         className="bg-green-300 rounded px-2 py-1 cursor-pointer"
-        onClick={() => setCount(count => count + 1)}
+        onClick={increment}
       >
-        Count is {count}
+        +
+      </button>
+      <button
+        type="button"
+        className="bg-blue-300 rounded px-2 py-1 cursor-pointer"
+        onClick={reset}
+      >
+        Reset
       </button>
     </>
   )
